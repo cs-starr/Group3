3b5fc86eb7538fd3586493f4c4186a2eec021800
angli3 <70265975+angli3@users.noreply.github.com>
2022-05-01 14:33:45 +0100

    Create calc_exons.py
    Exon calculation script - to offload from BL_api file

---
+++ cgi-biocomp2/bl/calc_exons.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
+"""
+Created on Thu Apr 28 11:05:49 2022
+This is the script for exon use calculation
+@author: angli
+"""
+
+def calc_exons(dna_seq, exon_locations, comp_strand, codon_start):
+    """
+    This function calculates the exons
+    dna_seq - string
+    exon_location - list of tuples
+    comp_string - boolean
+    codon_start - int
+    --------
+    Returns:
+    String containing the coding DNA sequence
+    """
+    
+    temp_exon_store = []
+    for (start,stop) in exon_locations:
+        if start[0] == "<" or start[0] == ">":
+            start = int(start[1:])
+        else:
+            start = int(start)
+        if stop[0] == "<" or stop[0] == ">":
+            stop = int(stop[1:])
+        else:
+            stop = int(stop)
+        temp_exon_store.append(dna_seq[start:(stop+1)])
+    working_exon_string = "".join(temp_exon_store)
+    
+    #Addressing whether the sequence begins after the given co-ordinates. 
+    #(Codon start is which nucleotide it start from with 1 being the co-ordinated given)
+    trim = (codon_start - 1) 
+    final_exon_str = ""
+    
+    if comp_strand == 1:
+        rev_ex_str = working_exon_string[::-1]
+        rev_list = []
+        final_exon_str = ""
+        for i in rev_ex_str:
+            if i == "c":
+                s = "g"
+            if i == "g":
+                s = "c"
+            if i == "a":
+                s = "t"
+            if i == "t":
+                s = "a"
+            rev_list.append(s)
+        final_exon_str = "".join(rev_list)
+    else:
+            final_exon_str = working_exon_string
+            
+    final_exon_str = final_exon_str[trim:]
+    
+    return(final_exon_str)
