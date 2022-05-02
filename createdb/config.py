24a7d54ae645450a0a9c6128dc450df6f33a76d4
jenny-416 <74254245+jenny-416@users.noreply.github.com>
2022-05-02 22:10:45 +0100

    Update config.py

--- createdb/config.py
+++ createdb/config.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+"""
+This file data contains the database connection info for the dbapi.
+Author: Janani Govindarasan
+Created on 4th April 2022
+"""
+
 mysql = {
     "host": "pandora",
     "user": "gj001",
