39ad58d26d8ebceb23095acdac642dc5eec61e74
angli3 <70265975+angli3@users.noreply.github.com>
2022-05-03 18:14:40 +0100

    Update calc_exons.py
    Updated docstring and finalised code sequence

--- cgi-biocomp2/bl/calc_exons.py
+++ cgi-biocomp2/bl/calc_exons.py
@@ -1,10 +1,23 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
+"""
+Created on Fri Apr 29 14:50:05 2022
+
+This is the script for calculation of the coding DNA sequence given input information on 
+the overall DNA sequence, exon locations, forward or complimentary strand location of the gene and 
+whether the uploaded sequence is offset.
+
+@author: angli
+"""
+
 def calc_exons(dna_seq, exon_locations, comp_strand, codon_start):
     """
-    This function calculates the exons
-    dna_seq - string
-    exon_location - list of tuples
-    comp_string - boolean
-    codon_start - int
+    This function calculates the exons when given information of:
+    dna_seq - str - the overall genomic sequence
+    exon_location - list of tuples of format ("string","string")
+    comp_string - boolean - 1 indicates that the coding sequence is present on the complementary strand
+    codon_start - int - the codon start offset, as indicated by the genbank file
     --------
     Returns:
     String containing the coding DNA sequence
@@ -19,12 +32,12 @@ def calc_exons(dna_seq, exon_locations, comp_strand, codon_start):
         if stop[0] == "<" or stop[0] == ">":
             stop = int(stop[1:])
         else:
-            stop = int(stop)-1
+            stop = int(stop)
         temp_exon_store.append(dna_seq[(start-1):(stop)])
     working_exon_string = "".join(temp_exon_store)
     
     #Addressing whether the sequence begins after the given co-ordinates. 
-    #(Codon start is which nucleotide it start from with 1 being the co-ordinated given)
+    #(Codon start is which nucleotide it start from with 1 being the co-ordinate given within te genbank file)
     trim = (codon_start - 1) 
     final_exon_str = ""
     
@@ -49,21 +62,3 @@ def calc_exons(dna_seq, exon_locations, comp_strand, codon_start):
     final_exon_str = final_exon_str[trim:]
     
     return(final_exon_str)
-
-"""
-#Test code:
-ds = "aggaaggcatgcggtgggccctactggtgcttctagctttcctgtctcctggtgagtacgctgcctacagagaggctcacaggttgggttttgttttgttttcttcttgaaaggggtgccatacaaaggaatacctcattgtattttgtgttgttcccattgcagccagtcagaaatcttccaacttggaagggagaacgaagtcagtcaccaggcagactgggtcatctgctgaaatcacttgcgatcttactgtaacaaataccttctacatccactggtacctacaccaggaggggaaggccccacagcgtcttctgtactatgacgtctccaccgcaagggatgtgttggaatcaggactcagtccaggaaagtattatactcatacacccaggaggtggagctggatattgagactgcaaaatctaattgaaaatgattctggggtctattactgtgccacctggcggacgaattattataagaaactctttggcagtggaacaacacttgttgtcacaggtaagtatcggaagaatacaacatttccaaggtaatagagggaaggcaggaaatgattaaactggaataatgt"
-el =  [("9","51"),("166",">525")]
-cs = 0
-csod = 1
-
-ds2 = "ctgcttgatggcaaagaaataaagcgactgaatgttcagtggctccgagcacacctgggcatcgtgtcccaggagcccatcctgtttgactgcagcattgctgagaacattgcctatggagacaacagccgggtggtgtcacaggaagagatcgtgagggcagcaaaggaggccaacatacatgccttcatcgagtcactgcctaat"
-el2 =  [("<1",">207")]
-cs2 = 0
-csod2 = 1
-print(calc_exons(ds,el,cs,csod))
-print(len(calc_exons(ds,el,cs,csod)))
-print("space-------")
-print(calc_exons(ds2,el2,cs2,csod2))
-print(len(calc_exons(ds2,el2,cs2,csod2)))
-"""
